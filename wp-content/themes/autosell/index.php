4cda4ff57c66760699e3436612bc1677eaa2687b
Alex <alex4jdm@gmail.com>
2018-08-27 20:18:18 +0300

    Added Roboto. First section is in process.

--- wp-content/themes/autosell/index.php
+++ wp-content/themes/autosell/index.php
@@ -2,7 +2,31 @@
 get_header();
 
 ?>
-hey
+
+<section id="first-section" style="background-image: url('<?php echo get_theme_file_uri('/img/background.png'); ?>');">
+    <div class="row main-section-container">
+        <div class="contact-us columns small-6 large-4">
+            <h2>
+                Купите автомобиль своей мечты
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, officia pariatur! Dolorum, in ex laudantium accusantium vitae aspernatur culpa ratione quod. Cumque nam quam molestiae corporis voluptatum molestias minus soluta.
+            </p>
+            <a href="#">
+                <div>
+                    Связаться с нами
+                </div>
+            </a>
+        </div>
+    
+    </div>
+
+
+</section>
+
+
+
+
 <?php 
 get_footer();
 
